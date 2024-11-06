23bbc5be1ce8632f4d70730bf5a7753a730f80b0
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-11-07 00:46:07 +0300

    add: enhance ChatPage layout with message display and send button

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -10,6 +10,63 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 60),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.only(right: 10, left: 10),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      Color.fromARGB(255, 225, 232, 235),
+                      Color.fromARGB(255, 197, 227, 244),
+                    ], begin: Alignment.topRight, end: Alignment.bottomLeft),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Chat",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff284a79)),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 35),
+              Center(
+                child: Text(
+                  "There is no message 🙁",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Color(0xff284a79),
+                    borderRadius: BorderRadius.circular(25)),
+                width: 200,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    "Send Message",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              SizedBox(height: 75)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
